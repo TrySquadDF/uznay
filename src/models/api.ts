68c7d345a7b843d630f6392958c8c0e53129917d
TrySquadDF <sjpdkedkdjkdkdjdmddd@gmail.com>
2022-10-14 23:22:14 +0300

    Global: added types and utils.

--- src/models/api.ts
+++ src/models/api.ts
@@ -1,24 +1,33 @@
 import { flow, types } from "mobx-state-tree";
-import { ResponsError } from "src/types/responsTypes";
+import { ResponsError, toServer } from "src/types/responsTypes";
+import { parameterAssembly } from "src/utils/parameterAssembly";
 import { apiResult, apiError } from "./apiModels";
 
-const fetchArticlesToServer = fetch(
-  `https://newsapi.org/v2/top-headlines?country=ru&apiKey=${
-    import.meta.env.VITE_API_KEY
-  }`
-)
-  .then((e) => {
-    if (e.ok) {
-      return e.json();
-    } else {
-      throw e;
-    }
-  })
-  .catch(async (e) => {
-    return await e.json().then((e: ResponsError) => {
-      throw e;
+const fetchArticlesToServer: toServer = async ({
+  country = "ru",
+  location = "/v2/top-headlines",
+  ...args
+}) => {
+  return fetch(
+    `${import.meta.env.VITE_API_URL}${parameterAssembly({
+      country,
+      location,
+      ...args,
+    })}&apiKey=${import.meta.env.VITE_API_KEY}`
+  )
+    .then((e) => {
+      if (e.ok) {
+        return e.json();
+      } else {
+        throw e;
+      }
+    })
+    .catch(async (e) => {
+      return await e.json().then((e: ResponsError) => {
+        throw e;
+      });
     });
-  });
+};
 
 const API = types
   .model("API", {
@@ -30,7 +39,7 @@ const API = types
     init: flow(function* fetchArticles() {
       try {
         self.loading = true;
-        const data = yield fetchArticlesToServer;
+        const data = yield fetchArticlesToServer({});
         self.result = { ...data };
         self.loading = false;
       } catch (e) {
--- src/types/responsTypes.ts
+++ src/types/responsTypes.ts
@@ -1,5 +1,18 @@
+import { category, country, location, q, sources } from "./apiParamsTypes";
+
 export interface ResponsError {
   code: string;
   status: "error";
   message: string;
 }
+
+export type query = {
+  location: location;
+  country: country;
+  category: category;
+  sources: sources;
+  q: q;
+  pageSize: number;
+  page: number;
+};
+export type toServer = (prams: Partial<query>) => Promise<any>;
