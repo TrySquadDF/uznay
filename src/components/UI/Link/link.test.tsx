452eacd8c68b40060f3df408e8b5fc332c86cffb
TrySquadDF <sjpdkedkdjkdkdjdmddd@gmail.com>
2022-10-17 23:53:08 +0300

    Global: update test

--- src/components/UI/Link/link.test.tsx
+++ src/components/UI/Link/link.test.tsx
@@ -1,24 +1,34 @@
 import "@testing-library/jest-dom";
 import { act, render, screen, userEvent } from "../../../utils/test-utils";
-import { describe, test } from "vitest";
+import { describe, test, vi } from "vitest";
 import { Link } from ".";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  MemoryRouter,
+  Router,
+} from "react-router-dom";
+import { createMemoryHistory } from "history";
 import React from "react";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Link to="/test" lable="test" />,
-  },
-]);
 
 describe("Link", async () => {
   it("Должен перенаправить на необходимую страницу", () => {
-    render(<RouterProvider router={router} />);
-    expect(screen.getByText("test")).toBeInTheDocument();
-    const link = screen.getByRole("link", {});
-    act(() => {
-      link.click();
-    });
+    const history = createMemoryHistory();
+    history.push = vi.fn();
+
+    const { getByText } = render(
+      <Router location={history.location} navigator={history}>
+        <Link to="/test" lable="test" />
+      </Router>
+    );
+
+    expect(getByText("test")).toBeInTheDocument();
+    userEvent.click();
+
+    // expect(screen.getByText("test")).toBeInTheDocument();
+    // const link = screen.getByRole("link", {});
+    // act(() => {
+    //   link.click();
+    // });
   });
 });
