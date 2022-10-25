ba279c49ed0b849b63a9de1efa47b5076f559520
TrySquadDF <sjpdkedkdjkdkdjdmddd@gmail.com>
2022-10-25 23:01:43 +0300

    Global: Coponent style and de-structuring

---
+++ src/components/Card/index.tsx
@@ -0,0 +1,122 @@
+import { Box } from "@/UI/Box";
+import { Text } from "@/UI/Text";
+import { Heading } from "@/UI/Heading";
+import { Image } from "@/UI/Image";
+import { To } from "@/UI/To";
+
+export const CardMin = ({
+  title,
+  disc,
+  date,
+  url,
+}: {
+  title: string;
+  disc: string | null;
+  date: string;
+  url: string;
+}) => {
+  return (
+    <Box
+      css={{
+        display: "flex",
+        flexDirection: "column",
+        maxWidth: "1200px",
+        "&:not(:first-child)": {
+          padding: "1rem 0",
+        },
+        "&:first-child": {
+          paddingBottom: "1rem",
+        },
+      }}
+    >
+      <Text
+        css={{
+          display: "flex",
+          padding: "0.5rem 0",
+          fontSize: "10pt",
+          color: "rgba(0, 0,0 , 0.5)",
+          fontWeight: "500",
+        }}
+      >
+        {new Date(date).toLocaleTimeString("ru", {
+          year: "numeric",
+          month: "long",
+          day: "numeric",
+          weekday: "long",
+        })}
+      </Text>
+      <Heading size="h3">{title}</Heading>
+      <Text
+        css={{
+          padding: "1rem 0",
+        }}
+      >
+        {disc}
+      </Text>
+      <To
+        href={url}
+        style="primary"
+        css={{
+          fontSize: "10pt",
+        }}
+      >
+        Узнать подробнее
+      </To>
+    </Box>
+  );
+};
+
+export const CardBig = ({
+  src,
+  title,
+  disc,
+  date,
+  url,
+}: {
+  src?: string | null;
+  title: string;
+  disc: string | null;
+  date: string;
+  url: string;
+}) => {
+  return (
+    <Box
+      css={{
+        maxWidth: "600px",
+      }}
+    >
+      <Image src={src} />
+      <Text
+        css={{
+          display: "flex",
+          padding: "0.5rem 0",
+          fontSize: "10pt",
+          color: "rgba(0, 0,0 , 0.5)",
+          fontWeight: "500",
+        }}
+      >
+        {new Date(date).toLocaleTimeString("ru", {
+          year: "numeric",
+          month: "long",
+          day: "numeric",
+          weekday: "long",
+        })}
+      </Text>
+      <Heading size="h2">{title}</Heading>
+
+      <Text
+        css={{
+          display: "flex",
+          paddingTop: "0.75rem",
+          fontSize: "11pt",
+          color: "rgba(0, 0,0 , 0.75)",
+        }}
+      >
+        {disc}
+      </Text>
+      <To href={url} style="primary">
+        Узнать подробнее
+      </To>
+    </Box>
+  );
+};
--- src/models/store.ts
+++ src/models/store.ts
@@ -13,7 +13,7 @@ const store = types
       self.APIV.init("/v2/everything", {
         q: "Russia",
         language: "ru",
-        pageSize: 5,
+        pageSize: 7,
       });
     },
   }));
